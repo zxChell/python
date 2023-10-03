5051e40f2b8507ab51c4443c319f4d1be16b3452
zxChell <144452384+zxChell@users.noreply.github.com>
2023-10-03 11:43:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+# Test 1
+
+# x = int(input('Введите число:' ))
+# print(x , x + 1 , x + 2)
+
+# x = int(input('Введите число:' ))
+# print(x , x + 1 , x + 2)
+#
+# x = int(input('Введите число:' ))
+# print(x , x + 1 , x + 2)
+#
+# x = int(input('Введите число:' ))
+# print(x , x + 1 , x + 2)
+#
+# x = int(input('Введите число:' ))
+# print(x , x + 1 , x + 2)
+
+# Test 2
+
+# x = int(input())
+# y = int(input())
+# c = int(input())
+# print(x + y + c)
+
+# Test 3
+
+# x = int(input())
+# print(x ** 3 , 6 + x ** 2)
+
+# Test 4
+
+# x = -15
+# y = 100
+#
+# x = 3 * (x + y) ** 3 + 275 * y ** 2 - 127 * x - 41
+# print(x)
+
+# Test 5
+
+# x = 20
+#
+# print("Следующее за числом", x, "число:" ,x + 1)
+# print("Для числа", x , "предыдущее число:", x - 1)
+
+# Test 6
+
+# x = 9900 + 55600 + 3999 + 2990
+# print(x * 3)
+
+# Test 7
+
+# a = int(input())
+# d = int(input())
+# n = int(input())
+# print(a+d*(n-1))
+
+# Test 8
+
+# x = 7
+#
+# print(x,"---",x * 2,"---",x * 3,"---",x * 4,"---",x * 5)
+#
+# x = 1
+#
+# print(x,"--- ",x + 1,"--- ",x + 2,"--- ",x + 3,"--- ",x + 4)
+
+# Test 9
+
